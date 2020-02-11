8487dd2f5a282bbc73e427bdbafa5d6cfac1fb48
Vadimkatr <vadimlisai@gmail.com>
2020-02-11 14:56:48 +0300

    rewrite service logic to use them without lib

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Vadimkatr/amqp_daemon
 go 1.13
 
 require (
+	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/kardianos/service v1.0.0
 	github.com/prometheus/client_golang v1.4.0
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
--- internal/app/serviceapp/serviceapp.go
+++ internal/app/serviceapp/serviceapp.go
@@ -3,21 +3,16 @@ package serviceapp
 import (
 	"context"
 	"fmt"
+	"github.com/streadway/amqp"
 	"os"
 	"time"
 
-	"github.com/kardianos/service"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/streadway/amqp"
 
 	"github.com/Vadimkatr/amqp_daemon/internal/app/amqpctl"
 	"github.com/Vadimkatr/amqp_daemon/internal/app/logger"
 )
-
-type CounterApp struct {
-	Logger service.Logger
-}
 
 var messageCounter = promauto.NewCounter(
 	prometheus.CounterOpts{
@@ -26,17 +21,16 @@ var messageCounter = promauto.NewCounter(
 	},
 )
 
-func (capp *CounterApp) Start(s service.Service) error {
-	capp.Logger.Infof("start service app manager.\n")
-
-	go capp.runTask()
-
-	return nil
+type ServiceApp struct {
+	Logger logger.Log
 }
 
-func (capp *CounterApp) runTask() {
-	lg := logger.CustomLogger{}
-	lg.Init()
+func (s *ServiceApp) Start(ctx context.Context) {
+	s.Logger.Info("service is started")
+	go s.run(ctx)
+}
+
+func (s *ServiceApp) run(ctx context.Context) {
 	cfg := amqpctl.Config{
 		DSN:                  os.Getenv("RABBITMQ_URL"), // by default is "amqp://guest:guest@localhost:5672/"
 		ReconnectionInterval: time.Duration(time.Minute * 10),
@@ -47,16 +41,16 @@ func (capp *CounterApp) runTask() {
 		SkipVerify:           false,
 		ServerName:           "",
 	}
-	conn, err := amqpctl.NewConnectionController(lg, cfg)
+	conn, err := amqpctl.NewConnectionController(s.Logger, cfg)
 	if err != nil {
-		capp.Logger.Errorf("error while creating amqpctl conn: %s", err)
+		s.Logger.Errorf("error while creating amqpctl conn: %s", err)
 		return
 	}
 	defer conn.Close()
 
-	ch, err := conn.OpenChannel(context.Background())
+	ch, err := conn.OpenChannel(ctx)
 	if err != nil {
-		capp.Logger.Errorf("error while opening channel: %s", err)
+		s.Logger.Errorf("error while opening channel: %s", err)
 		return
 	}
 	defer ch.Close()
@@ -70,12 +64,23 @@ func (capp *CounterApp) runTask() {
 		nil,
 	)
 	if err != nil {
-		capp.Logger.Errorf("error while queue declare: %s", err)
+		s.Logger.Errorf("error while queue declare: %s", err)
 		return
 	}
 
 	counter := 0
 	for {
+		select {
+		case <-ctx.Done():
+			{
+				s.Logger.Info("service start graceful shutdown")
+				return
+			}
+		default:
+			{
+			}
+		}
+
 		time.Sleep(1 * time.Second)
 		msg := fmt.Sprintf("%d", counter)
 		err = ch.Publish(
@@ -90,17 +95,18 @@ func (capp *CounterApp) runTask() {
 			},
 		)
 		if err != nil {
-			capp.Logger.Errorf("error while publishing msg: %s", err)
+			s.Logger.Errorf("error while publishing msg: %s", err)
 			return
 		}
 
 		messageCounter.Inc()
-		capp.Logger.Infof("send msg to: `%s`", msg)
+		s.Logger.Infof("send msg to: `%s`", msg)
 		counter++
 	}
 }
 
-func (capp *CounterApp) Stop(s service.Service) error {
-	capp.Logger.Infof("service is stopped.")
-	return nil
+func (s *ServiceApp) Stop(ctx context.Context) {
+	// context with timeout, to complete service task
+	<-ctx.Done()
+	s.Logger.Info("service is stopped.")
 }
